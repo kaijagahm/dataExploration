1ecc0dd3169de714b35b3ae6189941604decaf21
Kaija <kaija.gahm@gmail.com>
2022-10-10 16:07:13 -0700

    explicitly requiring geosphere for destPointRhumb

--- bboxFromRad.R
+++ bboxFromRad.R
@@ -1,8 +1,8 @@
 bboxFromRad <- function(lon, lat, kmDist){
-  top <- destPointRhumb(c(lon,lat),0,kmDist*1000)
-  left <- destPointRhumb(c(lon,lat),270,kmDist*1000)
-  bottom <- destPointRhumb(c(lon,lat),180,kmDist*1000)
-  right <- destPointRhumb(c(lon,lat),90,kmDist*1000)
+  top <- geosphere::destPointRhumb(c(lon,lat),0,kmDist*1000)
+  left <- geosphere::destPointRhumb(c(lon,lat),270,kmDist*1000)
+  bottom <- geosphere::destPointRhumb(c(lon,lat),180,kmDist*1000)
+  right <- geosphere::destPointRhumb(c(lon,lat),90,kmDist*1000)
   
   bottom_left <- c(left[1],bottom[2])
   top_right <-c(right[1],top[2])
