a7b2773fd1737d023340ba81a18369b4614d8956
Kaija Gahm <kgahm@ucla.edu>
2023-01-06 13:22:35 -0800

    some more goals, and changed read-in path of repr.Rda

--- spatsoc_data.table_reprex.R
+++ spatsoc_data.table_reprex.R
@@ -3,7 +3,7 @@ library(spatsoc)
 library(tidyverse)
 
 # A sample dataset
-load("data/repr.Rda")
+load("repr.Rda")
 
 # convert to a data.table for spatsoc
 data.table::setDT(repr)
@@ -19,7 +19,7 @@ names(repr) # this works! We have modified `repr` by reference, adding both `tim
 # Okay, so even though it's quite poorly documented in spatsoc, both of these functions have the ability to modify by reference.
 # BUT! If we make changes to the data between doing group_times and group_pts, group_pts won't successfully modify by reference.
 # A sample dataset
-load("data/repr.Rda")
+load("repr.Rda")
 data.table::setDT(repr)
 spatsoc::group_times(repr, datetime = "timestamp", threshold = "10 minutes")
 repr2 <- repr %>%
@@ -42,3 +42,9 @@ spatsoc::group_pts(repr2, threshold = 1000, id = "trackId", coords = c("utmE", "
 names(repr2)
 # wait WHAT??? Doing that did not fix the issue.
 # So it seems like the dplyr operation has broken this irreversibly and invisibly. I cannot figure out what was changed that is making this not work.
+# Goals:
+# 1. Figure out what criteria determine whether a data.table object can, or cannot, be modified by reference.
+# 2. Figure out how to see those criteria, because clearly class() and str() are not cutting it.
+# 3. Figure out how to transform a data.table object from one that can be modified by reference to one that cannot be modified by reference, and vice versa.
+# 4. Implement a check in `vultureUtils` to make sure the `group` column is present before any other computations are performed. (Pretty sure this check already exists, but I need to verify.)
+# 5. Send a PR to the spatsoc developers for increased documentation of the above, so this doesn't trip up other people.
